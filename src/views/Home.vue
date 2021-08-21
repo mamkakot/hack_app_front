c8aa48cf12451870269f94ae4fd220cf3fe3f0fb
mityakalinin <mitya.kalinin1@gmail.com>
2021-08-21 11:31:35 +0700

    changed navbar

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <Dashboard>
+</template>
+
+<script>
+import Dashboard from '@/components/Dashboard.vue';
+  export default {
+  components: {
+    Dashboard
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
