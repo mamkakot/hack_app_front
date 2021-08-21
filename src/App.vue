f4b9d91a47c998eafe2d6bb744cfe00df94bd3ba
mityakalinin <mitya.kalinin1@gmail.com>
2021-08-21 10:24:16 +0700

    add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,79 @@
 <template>
   <v-app>
+    
     <v-app-bar
       app
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-toolbar-title>Title</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Pososi</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item @click="drawer = false" v-for="view in views" :key="view.name">
+            <v-list-item-icon>
+              <v-icon>{{view.icon}}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{view.name}}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+        <AppealForm />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import AppealForm from '@/components/AppealForm.vue';
 export default {
+  
   name: 'App',
 
   components: {
-    HelloWorld,
+    AppealForm
   },
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
+    views: [
+      {
+        name: 'Home',
+        url: '/',
+        icon: 'mdi-home'
+      },
+
+      {
+        name: 'Pahome',
+        url: '/',
+        icon: 'mdi-account'
+      }
+    ]
   }),
 };
 </script>
